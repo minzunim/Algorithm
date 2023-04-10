331a99d3054fa380b7da0f0fd16f0d471864bc52
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-10 22:21:03 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -0,0 +1,9 @@
+def solution(s):
+    stack = []
+    for a in s.split():
+        if a != 'Z':
+            stack.append(int(a))
+        else:
+            stack.pop()
+        
+    return sum(stack)
