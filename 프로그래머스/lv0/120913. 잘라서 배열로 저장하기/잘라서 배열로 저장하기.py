4dfd03a20596a27e3f9a9f2ea1630e263b6255ea
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-11 10:57:54 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -13,3 +13,11 @@ def solution(my_str, n):
 # 경우를 나눠서 생각 - n의 배수인 경우, n의 배수가 아닌 경우
 # n = 2 / len(my_str) = 4 / [:2]를 2번 반복
 # n = 4 / len(my_str) = 16 / [:4]를 4번 반복
+
+'''
+[다른 풀이]
+def solution(my_str, n):
+    return [my_str[i: i + n] for i in range(0, len(my_str), n)]
+# range(시작, 끝, 증가폭)
+# 슬라이싱은 인덱스를 초과해도 에러가 발생하지 않음
+'''
