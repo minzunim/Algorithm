549f7807de803bc8621e51bf4ff88af429b2e1ab
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-11 13:05:34 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
+++ 프로그래머스/lv0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.py
@@ -1,2 +1,9 @@
 def solution(sides):
     return min(sides) + sum(sides) - max(sides) - 1
+
+# 입출력 예시
+# 가장 긴 변이 11인 경우 될 수 있는 나머지 한 변은 5, 6, 7, 8, 9, 10, 11 로 7개입니다.
+# max(sides) - (max(sides) - min(sides) + 1) + 1 => min(sides) - 1
+# 나머지 한 변이 가장 긴 변인 경우 될 수 있는 한 변은 12, 13, 14, 15, 16, 17 로 6개입니다.
+# sum(sides) - 1 - (max(sides) + 1) + 1 => sum(sides) - max(sides)
+# 두 경우의 수를 합치면 min(sides) + sum(sides) - max(sides) - 1
