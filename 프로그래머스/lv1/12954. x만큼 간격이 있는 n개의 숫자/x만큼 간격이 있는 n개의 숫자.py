1cd96258c9a08bf340a3fc5a24f1579b896ea892
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-12 23:27:58 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
+++ 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.py
@@ -5,3 +5,11 @@ def solution(x, n):
         answer.append(x)
         x += a
     return answer
+
+'''
+[다른 풀이]
+def number_generator(x, n):
+    # 함수를 완성하세요
+    return [i for i in range(x, x*n+1, x)
+# range 함수의 2번째 인자를 x*n+1로 설정하면 x가 n만큼 증가된 수까지만 계산할 수 있음 
+'''
