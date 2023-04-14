5662a7f47da96d6d9ed5ec828b1cf9357c9af3d0
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-14 21:50:30 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
+++ 프로그래머스/lv1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.py
@@ -8,3 +8,10 @@ def solution(arr, divisor):
         return answer
     else:
         return sorted(answer)
+
+'''
+[다른 풀이]
+def solution(arr, divisor): return sorted([n for n in arr if n % divisor == 0]) or [-1]
+# list 안에서 for 문 + if 문 사용하기
+# or 조건으로 앞의 값이 True가 아니면 [-1] 출력
+'''
