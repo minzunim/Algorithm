1b24b2fffb4d1a2d9fd3a30d9ca9cad37d48ee6e
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-14 10:43:23 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -8,3 +8,12 @@ def solution(a, b):
         for num in range(b, a+1):
             answer += num 
         return answer
+'''
+[다른 풀이]
+def solution(a, b):
+    if a > b:
+        a, b = b, a
+    return sum(range(a, b + 1))
+# a, b의 값을 비교해서 a가 크면 값을 서로 swap
+# range 함수로 합계 구함
+'''
