4620ac82bac391ef0a65acaa36bfbb08670fd614
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-15 10:02:10 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -7,3 +7,15 @@ def solution(n):
         else:
             answer += '박'
     return answer
+
+'''
+[다른 풀이1]
+def solution(n):
+    return "수박" * (n // 2) + "수" * (n % 2)
+# 2로 나눈 몫만큼 '수박'을 더하고 나머지만큼 '수'를 더하기
+
+[다른 풀이2]
+def solution(n):
+    return "".join(["수박"[i % 2] for i in range(n)])
+# '수박'이라는 단어를 '수' 또는 '박'으로 나눠 추가하는 배열을 만들고 join으로 문자열로 합치기
+'''
