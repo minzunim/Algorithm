69a90fd14ecf8ff7ce6a4ab22e2e2e7fce2b34a5
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-24 15:41:43 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 37.71 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,15 +1,15 @@
 def solution(n, m):
-    sol =[]
-    #최대 공약수
-    for i in range(min(n,m), 0, -1):
-        if n%i == 0 and m%i ==0:
-            sol.append(i)
+    result = []
+    # 최대공약수 (1부터 둘 중 작은 수까지 반복문, 최대공약수이므로 점점 작은 수로 반복)
+    for i in range(min(n, m), 0, -1):
+        if n % i == 0 and m % i == 0:
+            result.append(i)
             break
-
-    #최소 공배수
-    for j in range(max(n,m), (n*m)+1):
-        if j%n == 0 and j%m  ==0:
-
-            sol.append(j)
+    
+    # 최소공배수 (둘 중 큰 수부터 두 수를 곱한 수까지 반복)
+    for j in range(max(n, m), (n*m) + 1):
+        if j % n == 0 and j % m == 0:
+            result.append(j)
             break
-    return sol
+    return result
+    
