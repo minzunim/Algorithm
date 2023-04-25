19681472675a5cebd42895f3b7628d9f549f6ee7
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-25 09:52:56 +0900

    Comment: 풀이 추가

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -6,3 +6,7 @@ def solution(n):
         rev_base += str(q)
         
     return int(rev_base, 3)
+
+# 몫과 나머지를 함께 출력해주는 divmod() 사용
+# 몫이 0이 되기 전까지 나누고 나머지를 문자열로 붙이기
+# 주의사항: 나머지가 1의 자리부터 계산되므로 숫자가 거꾸로 출력
