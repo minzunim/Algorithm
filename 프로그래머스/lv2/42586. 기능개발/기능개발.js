9316c0764e2d1599685583c8a5be608922230746
minzunim <118159081+minzunim@users.noreply.github.com>
2023-04-28 10:10:53 +0900

    Update 기능개발.js

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -14,3 +14,11 @@ function solution(progresses, speeds) {
 
     return answer;
 }
+
+/*
+days: 각 기능의 배포 소요 시일을 순서대로 배열에 담음 (올림((100 - 현재 작업의 진도) / 개발 속도))
+maxDay: 현재 가장 오래 소요되는 기능의 소요 시일 (처음에는 제일 첫 번째 기능 == days[0])
+maxDay와 days의 각 요소들의 값을 비교
+maxDay보다 작거나 같으면 answer[j] 1 증가
+maxDay보다 크면 maxDay에 해당 요소를 저장하고 answer[++j] 1 증가
+*/
