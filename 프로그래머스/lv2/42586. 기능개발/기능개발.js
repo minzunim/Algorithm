b787be960bdacfbb8f4d4c2bf529a30a0fb45225
minzunim <118159081+minzunim@users.noreply.github.com>
2023-05-03 10:34:39 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42586. 기능개발/기능개발.js
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.js
@@ -1,24 +1,26 @@
 function solution(progresses, speeds) {
-    let answer = [0];
-    let days = progresses.map((progress, index) => Math.ceil((100 - progress) / speeds[index]));
-    let maxDay = days[0];
-
-    for(let i = 0, j = 0; i< days.length; i++){
-        if(days[i] <= maxDay) {
-            answer[j] += 1;
-        } else {
-            maxDay = days[i];
-            answer[++j] = 1;
-        }
+  const rest = []
+  const result = []
+  
+  for (let i = 0; i < progresses.length; i++) {
+    const restEl = Math.ceil((100 - progresses[i]) / speeds[i])
+    rest.push(restEl)
+  }
+  
+  let count = 1
+  let maxDay = rest[0]
+  
+  for (let j = 1; j < rest.length; j++) {
+    if (maxDay >= rest[j]) {
+      count += 1
+    } else {
+      result.push(count)
+      maxDay = rest[j]
+      count = 1
     }
-
-    return answer;
+  }
+  
+  result.push(count)
+  
+  return result
 }
-
-/*
-days: 각 기능의 배포 소요 시일을 순서대로 배열에 담음 (올림((100 - 현재 작업의 진도) / 개발 속도))
-maxDay: 현재 가장 오래 소요되는 기능의 소요 시일 (처음에는 제일 첫 번째 기능 == days[0])
-maxDay와 days의 각 요소들의 값을 비교
-maxDay보다 작거나 같으면 answer[j] 1 증가
-maxDay보다 크면 maxDay에 해당 요소를 저장하고 answer[++j] 1 증가
-*/
