8bca5a11355dec9bb8cb0035f1eaf74e28fddc0a
minzunim <118159081+minzunim@users.noreply.github.com>
2025-08-15 10:30:09 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.66 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=python3) 
 
 ### 성능 요약
 
-메모리: 27.1 MB, 시간: 45.80 ms
+메모리: 31.3 MB, 시간: 51.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 14일 22:09:32
+2025년 08월 15일 10:30:06
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,13 +1,10 @@
 def solution(arr):
-    # arr = [0, -9]
-    # 연속적으로 나타나는 숫자는 모두 제거
-    # 순서 유지
-    queue = []
-    prev = None
+    # 앞의 숫자와 같은지 확인
+    prev = arr[0]
+    queue = [prev]
     
-    for num in arr:
-        cur = num
-        if prev != cur:
-            queue.append(cur)
-            prev = cur
+    for num in arr[1:]:
+        if prev != num:
+            queue.append(num)
+        prev = num
     return queue
