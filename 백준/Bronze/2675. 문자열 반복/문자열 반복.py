42311d6dd442731994f1fd2ec1ca16f04914c2fb
minzunim <118159081+minzunim@users.noreply.github.com>
2025-10-15 20:40:05 +0900

    [Bronze II] Title: 문자열 반복, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.py
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -1,9 +1,7 @@
 t = int(input())
 
-for i in range(t):
+for _ in range(t):
     number, string = input().split()
-    new_string = ''
     for s in string:
-        new_string += int(number) * s
-    print(new_string)
-    
+        print(int(number) * s, end="")
+    print()
